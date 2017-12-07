de2f722093d9caad55651b6a486ad884daaa2abd
Bryan <bwasham@consultwithcase.com>
2017-12-07 13:12:13 -0500

    guide examples up to composing w/ component. Adding package json and .gitignore file

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
+    message: 'Hello Vue.js! You loaded page on ' + new Date().toLocaleString()
+  }
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue({
+  el: '#myTodoList',
+  data: {
+    todos: [{ text: 'Get AWS Certification' }, { text: 'Learn Vue' }, { text: 'take ultimate angular course' }]
+  }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
     message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('');
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vueski!'
   }
 });
